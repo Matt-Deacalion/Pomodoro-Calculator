e434160af15429f2829fea5bf89220e308c980b9
Matt Deacalion Stevens <matt@dirtymonkey.co.uk>
2014-06-10 14:59:34 +0100

    Add `_create_datetime` method to `PomodoroCalculator` class
    Also added a `requirements.txt` file for the new dependencies.

--- pomodoro_calculator/__init__.py
+++ pomodoro_calculator/__init__.py
@@ -1,5 +1,23 @@
+import datetime
+
+
 class PomodoroCalculator:
     """
     Calculates the number of pomodoros available in an amount of time.
     """
-    pass
+    def _create_datetime(self, time_string, tomorrow=False):
+        """
+        Takes a string in the format of 'HH:MM:SS' and returns a datetime. If
+        `tomorrow` is True it adds another day to the result.
+        """
+        replace_args = dict(zip(
+            ['hour', 'minute', 'second'],
+            [int(unit) for unit in time_string.split(':')],
+        ))
+
+        date = datetime.datetime.now().replace(**replace_args)
+
+        if tomorrow:
+            date + datetime.timedelta(days=1)
+
+        return date
--- pomodoro_calculator/test/test_main.py
+++ pomodoro_calculator/test/test_main.py
@@ -1,11 +1,56 @@
 import unittest
+from datetime import datetime
+from freezegun import freeze_time
 from pomodoro_calculator import PomodoroCalculator
 
 
 class PomodoroTest(unittest.TestCase):
+
+    def setUp(self):
+        self.calculator = PomodoroCalculator()
 
     def test_retriever_class_exists(self):
         """
         Does the `PomodoroCalculator` class exist?
         """
         self.assertTrue('PomodoroCalculator' in globals())
+
+    @freeze_time('2014-01-01 00:00:00')
+    def test_create_datetime_with_hours(self):
+        """
+        Does `_create_datetime` work correctly with only hours provided?
+        """
+        self.assertEqual(
+            self.calculator._create_datetime('15'),
+            datetime(2014, 1, 1, 15),
+        )
+
+    @freeze_time('2014-01-01 00:00:00')
+    def test_create_datetime_with_hours_and_minutes(self):
+        """
+        Does `_create_datetime` work correctly with hours and minutes?
+        """
+        self.assertEqual(
+            self.calculator._create_datetime('15:30'),
+            datetime(2014, 1, 1, 15, 30),
+        )
+
+    @freeze_time('2014-01-01 00:00:00')
+    def test_create_datetime_with_hours_minutes_and_seconds(self):
+        """
+        Does `_create_datetime` work correctly with hours, minutes and seconds?
+        """
+        self.assertEqual(
+            self.calculator._create_datetime('15:30:25'),
+            datetime(2014, 1, 1, 15, 30, 25),
+        )
+
+    @freeze_time('2014-01-02 00:00:00')
+    def test_create_datetime_with_different_day(self):
+        """
+        Does `_create_datetime` add another day is `tomorrow` is passed?
+        """
+        self.assertEqual(
+            self.calculator._create_datetime('15:30:25', tomorrow=True),
+            datetime(2014, 1, 2, 15, 30, 25),
+        )
