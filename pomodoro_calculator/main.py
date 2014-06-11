511e92e796224d8185a820d88d12d52c5479b739
Matt Deacalion Stevens <matt@dirtymonkey.co.uk>
2014-06-12 00:45:04 +0100

    Fix command line usage options

--- pomodoro_calculator/main.py
+++ pomodoro_calculator/main.py
@@ -7,9 +7,9 @@ Usage:
 Options:
   --version                   show program's version number and exit.
   -h, --help                  show this help message and exit.
-  -f, --from=<time>           calculate available pomodori from this time [default: now].
-  -b, --break=<minutes>       the amount of minutes between each pomodori [default: 5].
-  -l, --long-break=<minutes>  the amount of mintues between every five pomodori [default: 15].
+  -f, --from=<time>           calculate available Pomodori from this time [default: now].
+  -b, --break=<minutes>       the amount of minutes between each Pomodori [default: 5].
+  -l, --long-break=<minutes>  the amount of minutes between every four Pomodori [default: 15].
 """
 from docopt import docopt
 
