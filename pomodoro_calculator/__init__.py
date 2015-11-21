82113084367927f8df328a083d52aa390af3dcb4
ma3a <i6o6ep@gmail.com>
2015-11-21 15:48:01 +0400

    Added interval support to calc pomodoros based on delta time

--- pomodoro_calculator/__init__.py
+++ pomodoro_calculator/__init__.py
@@ -3,7 +3,7 @@ A pretty command line tool to calculate the number
 of Pomodori available between two points in time.
 """
 __author__ = 'Matt Deacalion Stevens'
-__version__ = '0.3.8'
+__version__ = '0.3.9'
 
 import datetime
 from itertools import cycle
@@ -15,7 +15,7 @@ class PomodoroCalculator:
     """
 
     def __init__(self, end, start='now', short_break=5, long_break=15,
-                 pomodoro_length=25, group_length=4):
+                 pomodoro_length=25, group_length=4, time_interval=False):
         self.pomodoro_length_seconds = pomodoro_length * 60
 
         if start == 'now':
@@ -26,11 +26,13 @@ class PomodoroCalculator:
             self.end = self._create_datetime(end, tomorrow=True)
         else:
             self.end = self._create_datetime(end)
-
+        
         self.start = self._create_datetime(start)
         self.group_length = group_length
         self.short_break = short_break
         self.long_break = long_break
+        if time_interval:
+            self.end = self._create_datetime_with_interval(self.start, end)
 
     @property
     def short_break_seconds(self):
@@ -60,21 +62,36 @@ class PomodoroCalculator:
         """
         return [int(i) for i in a.split(':')] > [int(i) for i in b.split(':')]
 
-    def _create_datetime(self, time_string, tomorrow=False):
+    def _create_time_dict(self, time_string):
+        time_dict = dict(zip(
+            ['hour', 'minute', 'second'],
+            [int(unit) for unit in time_string.split(':')],
+        ))
+        return time_dict
+
+    def _create_datetime(self, time_string, tomorrow=False, interval=False):
         """
         Takes a string in the format of 'HH:MM:SS' and returns a datetime. If
         `tomorrow` is True it adds another day to the result.
         """
-        replace_args = dict(zip(
-            ['hour', 'minute', 'second'],
-            [int(unit) for unit in time_string.split(':')],
-        ))
+        replace_args = self._create_time_dict(time_string)
 
         date = datetime.datetime.now().replace(**replace_args)
 
         if tomorrow:
             date += datetime.timedelta(days=1)
 
+        return date
+
+    def _create_datetime_with_interval(self, date_from, time_interval_string):
+        """
+        Takes a date and time_interval string in the format of 'HH:MM' 
+        and returns a datetime.
+        """
+        interval_args = self._create_time_dict(time_interval_string)
+
+        date = date_from + datetime.timedelta(hours=interval_args['hour'], 
+                                            minutes=interval_args['minute'])
         return date
 
     def _get_item(self, offset, item_type):
