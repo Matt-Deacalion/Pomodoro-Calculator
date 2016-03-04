9159b1f38a950859de6f363a450baa3bd094b715
Matt Deacalion Stevens <matt@dirtymonkey.co.uk>
2016-03-04 13:44:37 +0000

    :shipit: Bump version to `1.0.0`

--- pomodoro_calculator/__init__.py
+++ pomodoro_calculator/__init__.py
@@ -3,7 +3,7 @@ A pretty command line tool to calculate the number
 of Pomodori available between two points in time.
 """
 __author__ = 'Matt Deacalion Stevens'
-__version__ = '0.3.9'
+__version__ = '1.0.0'
 
 import datetime
 from itertools import cycle
